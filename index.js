be468bfbd9e125d3a5ad8016f20e16efbbfe69aa
Botlhale Setou <botlhale.setou@gmail.com>
2022-09-26 23:39:25 +0200

    Modularize Book and BookLibrary classes; Complete basic functionality

--- index.js
+++ index.js
@@ -1,155 +1,93 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable prefer-template */
 
-class BookLibrary {
-  constructor() {
-    this.booksArr = [];
-    this.add = (Obj) => {
-      this.booksArr.push(Obj);
-    };
-    this.remove = (Obj) => {
-      this.booksArr.splice(Obj, 1);
-    };
-  }
+import BookLibrary from "./modules/BookLibrary.js";
+
+const listRadio = document.querySelector('#listLabel');
+const addNewRadio = document.querySelector('#addNewLabel');
+const contactRadio = document.querySelector('#contactLabel');
+const listTab = document.querySelector('#listTab');
+const addNewTab = document.querySelector('#addNewTab');
+const contactTab = document.querySelector('#contactTab');
+const libraryDiv = document.querySelector('#book');
+const titleInput = document.querySelector('#title');
+const authorInput = document.querySelector('#author');
+const addBtn = document.querySelector('#addBtn');
+
+const library = new BookLibrary;
+let allRmvBtns = document.querySelectorAll('.remove-btn');
+
+const refresh = (booksDiv) => {
+  booksDiv.innerHTML = '';
+
+  library.arrBooks.forEach((book) => {
+    const bookDiv = document.createElement('div');
+    const xText = document.createElement('p');
+    const rmvBtn = document.createElement('button');
+    
+    xText.innerHTML = `"${book.title}" by ${book.author}`;
+    rmvBtn.innerHTML = 'Remove';
+
+    bookDiv.classList.add('book-container');
+    xText.classList.add('book-title');
+    rmvBtn.classList.add('remove-btn');
+
+    bookDiv.append(xText);
+    bookDiv.append(rmvBtn);
+    booksDiv.append(bookDiv);
+  })
 }
 
-const library = new BookLibrary();
+listRadio.addEventListener('click', () => {
+  listTab.style.display = 'block';
+  addNewTab.style.display = 'none';
+  contactTab.style.display = 'none';
+})
 
-const addBook = document.getElementById('book');
+addNewRadio.addEventListener('click', () => {
+  listTab.style.display = 'none';
+  addNewTab.style.display = 'block';
+  contactTab.style.display = 'none';
+})
 
-function drawOnScreen(obj) {
-  const stringVal = obj.title;
-  const idString = stringVal.replace(/\s/g, '');
-  const bookDiv = document.createElement('div');
-  const bookTitle = document.createElement('p');
-  const removeBtn = document.createElement('button');
-  const rmv = 'Rmv';
-  const rmvBtn = idString + rmv;
+contactRadio.addEventListener('click', () => {
+  listTab.style.display = 'none';
+  addNewTab.style.display = 'none';
+  contactTab.style.display = 'flex';
+})
 
-  bookDiv.classList.add('book-container');
-  bookTitle.classList.add('book-title');
-  removeBtn.classList.add('remove-btn');
-  removeBtn.innerHTML = 'Remove';
-  removeBtn.setAttribute('onclick', 'removeBook(this.id)');
-
-  bookTitle.innerHTML = '"' + obj.title + '" by ' + obj.author;
-
-  bookDiv.setAttribute('id', idString);
-  removeBtn.setAttribute('id', rmvBtn);
-
-  bookDiv.appendChild(bookTitle);
-  bookDiv.appendChild(removeBtn);
-  addBook.appendChild(bookDiv);
-}
-
-function addNewBook() {
-  const bookEntries = {
-    title: null,
-    author: null,
-  };
-  const titleInput = document.getElementById('title');
-  const authorInput = document.getElementById('author');
-
-  bookEntries.title = titleInput.value;
-  bookEntries.author = authorInput.value;
-  library.add(bookEntries);
+addBtn.addEventListener('click', () => {
+  library.add(titleInput.value, authorInput.value);
 
   titleInput.value = '';
   authorInput.value = '';
 
-  window.localStorage.setItem('books', JSON.stringify(library.booksArr));
+  refresh(libraryDiv);
+  updateRMV();
 
-  drawOnScreen(bookEntries);
-}
+  window.localStorage.setItem('books', JSON.stringify(library.arrBooks));
+})
 
-function removeBook(id) {
-  const rmv = 'Rmv';
-  for (let i = 0; i < addBook.children.length; i += 1) {
-    const selected = addBook.children[i];
-    if (selected.id + rmv === id) {
-      addBook.removeChild(selected);
+const updateRMV = () => {
+  allRmvBtns = document.querySelectorAll('.remove-btn');
+  for (let i = 0; i < allRmvBtns.length; i++) {
+    allRmvBtns[i].addEventListener('click', () => {
       library.remove(i);
-      window.localStorage.setItem('books', JSON.stringify(library.booksArr));
-    }
+
+      refresh(libraryDiv);
+      updateRMV();
+
+      window.localStorage.setItem('books', JSON.stringify(library.arrBooks));      
+    });  
   }
 }
 
 window.addEventListener('load', () => {
   const localStorageItem = window.localStorage.getItem('books');
   if (localStorageItem) {
-    library.booksArr = JSON.parse(localStorageItem);
-    library.booksArr.forEach((element) => {
-      drawOnScreen(element);
-    });
+    library.arrBooks = JSON.parse(localStorageItem);
+    
+    refresh(libraryDiv);
+    updateRMV();
   }
-});
-
-const listTab = document.getElementById('listTab');
-const addNewTab = document.getElementById('addNewTab');
-const contactTab = document.getElementById('contactTab');
-
-function selectList() {
-  listTab.style.display = 'block';
-  addNewTab.style.display = 'none';
-  contactTab.style.display = 'none'
-}
-
-function selectAdd() {
-  listTab.style.display = 'none';
-  addNewTab.style.display = 'block';
-  contactTab.style.display = 'none'
-}
-
-function selectContact() {
-  listTab.style.display = 'none';
-  addNewTab.style.display = 'none';
-  contactTab.style.display = 'flex'
-}
-
-const clock = document.getElementById("clock-time");
-const ante = document.getElementById("clock-ampm");
-
-function myHour() {
-  const time = new Date();
-  let hours = time.getHours().toString();
-  let minutes = time.getMinutes().toString();
-  let seconds = time.getSeconds().toString();
-  let antem = time.toLocaleTimeString().includes("PM");
-
-  if (hours > 12) {
-      hours -= 12;
-  };
-
-  if (hours == 0) {
-      hours = 12;
-  };
-
-  if (hours.toString().length < 2) {
-      hours = "0" + hours;
-  };
-
-  if (antem == true) {
-      ante.textContent = "PM";
-  } else {
-      ante.textContent = "AM";
-  };
-
-  if (minutes.length < 2) {
-      minutes = "0" + minutes;
-  };
-
-  if (seconds.length < 2) {
-      seconds = "0" + seconds;
-  };
-
-  setTimeout(myHour, 1000);
-
-  clock.textContent = hours + ":" + minutes + ":" + seconds;
-};
-
-myHour();
-
-window.addEventListener("change", function tryMe(event) {
-  invoker = event.target.id;
-  textUpdate(invoker);
 });
